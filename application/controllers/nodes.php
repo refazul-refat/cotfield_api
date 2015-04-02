b10d0abf8df47d6428368b7d5a9247aed39364bb
Refazul Refat <refazul.refat@gmail.com>
2015-04-02 11:16:55 +0600

    basic tree

---
+++ application/controllers/nodes.php
@@ -0,0 +1,34 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Nodes extends CI_Controller {
+	
+	public function index(){
+		
+		$id=$this->uri->segment(2,FALSE);
+		
+		if(!$id){
+			$id=$this->input->get_post('id');
+		}
+		
+		$this->db->select('*');
+		$this->db->from('tree');
+		$this->db->where('parent',$id);
+
+		$roots=$this->db->get()->result();
+		foreach($roots as &$item){
+			if($item->item_type=='folder'  || $item->item_type=='project'){
+				$this->db->select('name');
+				$this->db->from($item->item_type.'s');
+				$this->db->where('id',$item->item_id);
+				
+				$item->name=$this->db->get()->row()->name;
+				$item->isParent=true;
+			}
+			else $item->name=$item->item_type;
+		}
+		echo json_encode($roots);die();
+	}
+	public function _remap(){
+		$this->index();
+	}
+}

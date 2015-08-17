0b63654658e90d1b8db43fb3ca2063104b623886
Refazul <refazul@Refazuls-MacBook-Pro.local>
2015-08-17 22:13:23 +0600

    __config__

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'refazul';
+$db['default']['password'] = 'zxcvbnm';
+$db['default']['database'] = 'cotfield_api';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
